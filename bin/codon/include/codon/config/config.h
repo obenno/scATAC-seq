a42b9e54f86fab7ba9e1556c8a1640bedf754db2
xzx <obennoname@gmail.com>
2024-04-02 19:49:23 +0800

    Removed codon environment code

--- bin/codon/include/codon/config/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define CODON_VERSION       "0.16.3"
-#define CODON_VERSION_MAJOR 0
-#define CODON_VERSION_MINOR 16
-#define CODON_VERSION_PATCH 3
--- bin/codon/python/codon/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2022-2023 Exaloop Inc. <https://exaloop.io>
-
-__all__ = ["jit", "convert", "JITError"]
-
-from .decorator import jit, convert, JITError
